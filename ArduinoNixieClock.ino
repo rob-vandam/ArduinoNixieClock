7d78567c510e72fbdd58ba23656c688a7cd26748
Rob van Dam <rob_vandam@gmx.com>
2020-08-23 19:19:30 +0200

    Adding ds3231 rtc

--- ArduinoNixieClock.ino
+++ ArduinoNixieClock.ino
@@ -1,6 +1,9 @@
 #include <Adafruit_NeoPixel.h>
 #define PIN 7
 #define NUMPIXELS 1
+#include "RTClib.h"
+
+RTC_DS3231 rtc;
 Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_RGB + NEO_KHZ800);
 uint32_t hsv_color = 0;
 unsigned long previousMillis = 0;
@@ -9,15 +12,44 @@ long AnimationDelay = 12000;
 void setup() {
   Serial.begin(9600);
   pixels.begin();
+ if (! rtc.begin()) {
+   Serial.println("Couldn't find RTC");
+   Serial.flush();
+   abort();
+ }
+   if (rtc.lostPower()) {
+    Serial.println("RTC lost power, let's set the time!");
+    // When time needs to be set on a new device, or after a power loss, the
+    // following line sets the RTC to the date & time this sketch was compiled
+    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+    // This line sets the RTC with an explicit date & time, for example to set
+    // January 21, 2014 at 3am you would call:
+    // rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
+  }
 }
 
 void loop() {
+DateTime now = rtc.now();
 unsigned long currentMillis = millis();
 if (currentMillis - previousMillis >= AnimationDelay)
   {
   previousMillis = currentMillis;
   ledanimation();
   }
+  Serial.print(now.year(), DEC);
+    Serial.print('/');
+    Serial.print(now.month(), DEC);
+    Serial.print('/');
+    Serial.print(now.day(), DEC);
+    Serial.print(" (");
+  //  Serial.print(daysOfTheWeek[now.dayOfTheWeek()]);
+    Serial.print(") ");
+    Serial.print(now.hour(), DEC);
+    Serial.print(':');
+    Serial.print(now.minute(), DEC);
+    Serial.print(':');
+    Serial.print(now.second(), DEC);
+    Serial.println();
 //delay(3000);
 }
 
